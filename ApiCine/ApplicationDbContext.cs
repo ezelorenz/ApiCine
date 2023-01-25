3bdda6c43fc9c7ecc772fb33bac77dbafc9675bb
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-01-25 11:51:56 -0300

    Configuracion de DbContext para mapear entidad

---
+++ ApiCine/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiCine.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCine
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        { 
+
+        }
+
+        public DbSet<Genero> Generos => Set<Genero>();
+    }
+}
---
+++ ApiCine/Entidades/Genero.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiCine.Entidades
+{
+    public class Genero
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+    }
+}
